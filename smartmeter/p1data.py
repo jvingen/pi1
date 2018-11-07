38bf22fcefeab7e8372752604abeeafce250ac4d
joep <j.v.ingen@gmail.com>
2018-11-07 23:27:14 +0100

    p1data.py: Added OBIS_CODES as a dictionary

--- smartmeter/p1data.py
+++ smartmeter/p1data.py
@@ -3,8 +3,79 @@ class P1DataStructure:
     A class representing the P1 data structure
     """
 
-    OBIS = {
-
+    OBIS_CODES = {
+        "0-0:96.1.1":
+            {
+                "description": "Serial number",
+                "value_regex": r"[A-Z0-9]+",
+                "type": "str"
+            },
+        "1-0:1.8.1":
+            {
+                "description": "Meter Reading electricity delivered to client (Tariff 1) in 0,001 kWh",
+                "value_regex": r"[0-9]+\.[0-9]{3}",
+                "type": "float"
+            },
+        "1-0:1.8.2":
+            {
+                "description": "Meter Reading electricity delivered to client (Tariff 2) in 0,001 kWh",
+                "value_regex": r"[0-9]+\.[0-9]{3}",
+                "type": "float"
+            },
+        "1-0:2.8.1":
+            {
+                "description": "Meter Reading electricity delivered by client (Tariff 1) in 0,001 kWh",
+                "value_regex": r"[0-9]+\.[0-9]{3}",
+                "type": "float"
+            },
+        "1-0:2.8.2":
+            {
+                "description": "Meter Reading electricity delivered by client (Tariff 2) in 0,001 kWh",
+                "value_regex": r"[0-9]+\.[0-9]{3}",
+                "type": "float"
+            },
+        "0-0:96.14.0":
+            {
+                "description": "Tariff indicator",
+                "value_regex": r"[0-9]{4}",
+                "type": "int"
+            },
+        "1-0:1.7.0":
+            {
+                "description": "Actual electricity power delivered(+P) in 1 Watt resolution",
+                "value_regex": r"[0-9]+\.[0-9]{2}",
+                "type": "float"
+            },
+        "1-0:2.7.0":
+            {
+                "description": "Actual electricity power received(-P) in 1 Watt resolution",
+                "value_regex": r"[0-9]+\.[0-9]{2}",
+                "type": "float"
+            },
+        "0-0:17.0.0":
+            {
+                "description": "Maximum power per phase in kW resolution",
+                "value_regex": r"[0-9]+\.[0-9]{2}",
+                "type": "float"
+            },
+        "0-0:96.3.10":
+            {
+                "description": "Switch position (1 is on)",
+                "value_regex": r"[0-9]",
+                "type": "int"
+            },
+        "0-0:96.13.1":
+            {
+                "description": "Message numeric",
+                "value_regex": r"[0-9]*",
+                "type": "int"
+            },
+        "0-0:96.13.0":
+            {
+                "description": "Message string",
+                "value_regex": r".*",
+                "type": "str"
+            }
     }
 
     def __init__(self):
