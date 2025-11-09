735f5f267aa5287dc4e0283cecd651e59324048f
Joep <j.v.ingen@gmail.com>
2025-11-09 22:55:39 +0100

    smartmeter-lib: Add Redis connector

---
+++ packages/smartmeter-lib/src/smartmeter/lib/redis/__init__.py
@@ -0,0 +1,16 @@
+import redis
+
+from .data import DEFAULT_DB, DEFAULT_HOST, DEFAULT_PORT
+def connect(
+        host: str | None = None,
+        port: int | None = None,
+        db: int | None = None,
+
+):
+    if host is None:
+        host = DEFAULT_HOST
+    if port is None:
+        port = DEFAULT_PORT
+    if db is None:
+        db = DEFAULT_DB
+    return redis.Redis(host=host, port=port, db=db)
